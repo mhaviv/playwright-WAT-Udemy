f8e1d9a7485d8dac2b9b9e72cfe546d1c35d60d3
Michael Haviv <michael.haviv@foxnews.com>
2021-01-04 22:17:02 -0500

    Updates to lesson 7

--- 07_Waits_Timeouts.js
+++ 07_Waits_Timeouts.js
@@ -4,7 +4,9 @@ const expect = require('expect');
 (async () => {
     const browser = await chromium.launch({ headless:false, slowMo: 50 })
     const context = await browser.newContext()
+    context.setDefaultTimeout(45000) // updates the default maximum time for all the methods on all the pages
     const page = await context.newPage()
+    page.setDefaultTimeout(45000) // updates the default maximum time for all the methods on the page excepting timeout options
 
     // Check we are on the right page
     await page.goto('https://react-redux.realworld.io/#/login')
@@ -14,14 +16,21 @@ const expect = require('expect');
     await page.fill('input[type = "email"]', 'qacamp.acad@gmail.com')
     await page.press('input[type = "email"]', 'Tab')
     await page.type('input[type = "password"]', 'test12345')
-    await page.click('form >> "Sign in"')
+    await page.click('form >> "Sign in"', {timeout: 45000}) // update default timeout of 30 seconds to 45 seconds
 
     // await page.waitForTimeout(5000) // Static wait (not recommended since it makes tests flaky since you never know if it'll finish loading by 5 seconds)
     // await page.waitForSelector('.ion-compose') // Waiting for element to become visible
-    await Promise.all([
-        page.waitForNavigation(),
-        await page.click('form >> "Sign in"')
+    // await Promise.all([
+    //     page.waitForNavigation(), // will resolve when the page navigates to the new url
+    //     await page.click('form >> "Sign in"')
+    // ])
+
+    const [newPage]  = await Promise.all([
+        context.waitForEvent('page') // Wait for tab to open
+        page.click('a[target="_blank"]') // Opens a new tab
     ])
+
+    await page.waitForLoadState() // wait until the page is fully loaded
 
     await browser.close()
 }) ()
